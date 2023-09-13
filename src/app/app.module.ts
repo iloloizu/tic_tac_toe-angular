d50021199f417297bcb3327b47e158b263fe63aa
Ilolo Izu <iloloizu97@gmail.com>
2023-09-13 12:13:53 -0400

    update 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule,
